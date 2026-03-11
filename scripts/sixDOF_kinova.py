30cfb066e42fa38c71eb6e477e885464468ecafc
GrantSRechtin <gsullivanr@icloud.com>
2026-03-11 11:20:39 -0400

    current

--- scripts/sixDOF_kinova.py
+++ scripts/sixDOF_kinova.py
@@ -12,6 +12,66 @@ class KinovaRobot(KinovaRobotTemplate):
     def __init__(self):
         super().__init__()
 
+    def calc_inverse_kinematics(
+        self, ee: ut.EndEffector, joint_values: List[float], soln: int = 0
+    ) -> List[float]:
+        """
+        Compute an analytical inverse kinematics solution.
+
+        Given a desired end-effector pose, this method computes a set of joint
+        angles that achieve the pose, if a valid solution exists.
+
+        Args:
+            ee (EndEffector): Desired end-effector pose.
+            joint_values (List[float]): Initial or previous joint angles, used
+                for solution selection or continuity.
+            soln (int, optional): Solution branch index (e.g., elbow-up vs
+                elbow-down). Defaults to 0.
+
+        Returns:
+            list[float]: Joint angles [theta1, theta2] in radians that achieve
+            the desired end-effector pose.
+        """
+        l1,l2,l3,l4,l5,l6,l7 = self.l1,self.l2,self.l3,self.l4,self.l5,self.l6,self.l7
+
+        curr_ee, _ = self.calc_forward_kinematics(new_joint_values)
+        d6 = l6+l7
+
+        w_ee = ee - d6*ee*np.transpose([0,0,1])
+        xw = w_ee.x
+        yw = w_ee.x
+        zw = w_ee.z
+
+        th1 = tan(yw/xw)
+
+        x1 = sqrt(xw^2 + yw^2)
+        y1 = zw-l1
+        L = sqrt(x1^2 + y1^2)
+
+        beta = acos(l2^2+(l3+l4)^2-L^2,2*l2*(l3+l4))
+        th3 = np.pi - beta
+
+        alpha = atan((l3+l4)*sin(th3),l2+(l3+l4)*cos(th3))
+        gamma = atan(y1/x1)
+
+        th2 = gamma-alpha
+        
+        dh_table = np.array([
+            [0,0,0,pi],
+            [th1,-self.l2, 0, pi/2],
+            [th2 + pi/2,0,-self.l3,pi],
+            [th3 + pi/2,0,0,pi/2],
+        ])
+
+        H03 = self.dh_to_H(dh_table)
+        R03 = H03[:3, :3]
+
+        R36 = ut.euler_to_rotm((curr_ee.rotx,curr_ee.roty,curr_ee.rotz)).T @ R03
+        
+
+        new_joint_values = joint_values.copy()
+        return new_joint_values
+
     def calc_numerical_ik(
         self, ee: ut.EndEffector, joint_values: List[float], tol: float = 0.01, ilimit: int = 100
     ) -> List[float]:
